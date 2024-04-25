b9c10bff90118659cb7cb699c4c625ae12909c60
oructuran0 <orctrn193@gmail.com>
2024-04-25 16:00:06 +0300

    Dort Islem test
    test olarak yapildi. kaldirilabilir.

---
+++ OgrenciNotMVC/OgrenciNotMVC/Controllers/DortIslemController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OgrenciNotMVC.Controllers
+{
+    public class DortIslemController : Controller
+    {
+        // GET: DortIslem
+        public ActionResult Index(double s1=0, double s2 =0)
+        {
+            double toplama = s1+s2;
+            double cikarma = s1-s2;
+            double carpma = s1*s2;
+            double bolme = s1/s2;
+            ViewBag.toplama = toplama;
+            ViewBag.cikarma = cikarma;
+            ViewBag.carpma = carpma;
+            ViewBag.bolme = bolme;
+            return View();
+        }
+    }
+}
